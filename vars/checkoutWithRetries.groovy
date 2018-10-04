8c6193746e2c02fe87b1d9cac8f46cba1d7e37db
Tom Owens <tom@towens.com>
2018-10-04 17:26:51 -0600

    Bring the functions back as global vars.

---
+++ vars/checkoutWithRetries.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+
+def call(Integer retryCount = 10) {
+    while (retryCount > 0) {
+        try {
+            if (retryCount == 10) {
+                throw new Exception('intentional test condition reached')
+            }
+            checkout scm
+            retryCount = 0
+        }
+        catch (Exception e) {
+            msg = e.getMessage()
+            if ((retryCount % 5) == 0) {
+                echo "Checkout scm failed due to ${msg}.  Retrying"
+            }
+            retryCount--
+            sleep(60)
+        }
+    }
+}
