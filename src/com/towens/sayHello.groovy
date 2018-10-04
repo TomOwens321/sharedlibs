8c7d0153d2f2351eec943d2e97097aa57390cc1b
Tom Owens <tom@towens.com>
2018-10-04 08:23:41 -0600

    Recreate sayHello as a true groovy script

---
+++ src/com/towens/sayHello.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+package com.towens
+
+def sayHello(String name = 'Human') {
+    echo "Hello, ${name}."
+}
--- vars/sayHello.groovy
+++
@@ -1,5 +0,0 @@
-def call(String name = 'Human') {
-    // Any valid steps can be called here
-    echo "Hello, ${name}."
-}
-println('Hi')
